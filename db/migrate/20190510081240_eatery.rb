610c20409b8c8dd3a43304430974c1209031d853
齊藤亮将 <s.ryosuke@saitouryoushounoMacBook-puro.local>
2019-05-10 23:27:16 +0900

    detail

--- db/migrate/20190510081240_eatery.rb
+++
@@ -1,7 +0,0 @@
-class Eatery < ActiveRecord::Migration[5.2]
-  def change
-
-    add_column :eateries, :tel, :string
-
-  end
-end
--- db/migrate/20190510084134_delete_eatery.rb
+++
@@ -1,5 +0,0 @@
-class DeleteEatery < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :eateries
-  end
-end
