bf5d6a47bf2e69c74fc74e1d857a866aae9acda9
齊藤亮将 <s.ryosuke@13351125220.ap.cc.tsukuba.ac.jp>
2019-05-17 17:17:44 +0900

    ユーザ登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_085312) do
+ActiveRecord::Schema.define(version: 2019_05_17_080229) do
 
   create_table "eateries", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2019_05_10_085312) do
     t.string "close"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
