425b6ba6cc9851681e96afd96663d0e975ce35e1
齊藤亮将 <s.ryosuke@13351125220.ap.cc.tsukuba.ac.jp>
2019-05-17 17:52:47 +0900

    ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'eateries/new'
   get "lunch/go",to:"lunch#go"
 
   root "lunch#go"
 
   post 'eateries/create'
+
+  get '/login', to:'sessions#new'
+  post 'sessions/create'
 end
